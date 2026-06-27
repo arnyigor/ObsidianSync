9d3fa5cc613e0342d50547bffb177f5436b2b5ea
Arnyigor <arnyigor@gmail.com>
2026-06-28 00:58:03 +0500

    init commit

---
+++ desktopApp/src/main/kotlin/ru/arny/obsidiansync/main.kt
@@ -0,0 +1,13 @@
+package ru.arny.obsidiansync
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "ObsidianSync",
+    ) {
+        App()
+    }
+}
---
+++ shared/src/commonMain/kotlin/ru/arny/obsidiansync/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package ru.arny.obsidiansync
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
